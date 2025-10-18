9c71f62da15cff414aef3bf1d60ec3024996fb3c
Rushi ahire <rushiahire9567@gmail.com>
2025-10-18 13:05:04 +0530

    fix: UI changes

--- components/AddPatient/UserBox.tsx
+++ components/AddPatient/UserBox.tsx
@@ -1,8 +1,15 @@
 import styles from "@/styles/addPatient/addPatient.module.css";
+import Image from "next/image";
 
 const UserBox = ({ avatar, name, phone }: any) => (
   <div className={`${styles.sectionBox} ps-3`}>
-    <img className={styles.avatar} src={avatar} alt={name} />
+    <Image
+      className={styles.avatar}
+      src={avatar}
+      alt={name}
+      width={38}
+      height={38}
+    />
     <div className={styles.info}>
       <div className="fs-14 fw-600 mb-0">{name}</div>
       <div className={`fs-11 ${styles.infoPhone}`}>{phone}</div>
